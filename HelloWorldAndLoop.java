3d07b51c34a937fed06877c35f715c18046f041e
ssarkar4289 <ssarkar4289@students.mc3.edu>
2023-12-07 16:01:46 -0500

    Created loop
    I created hello world print line and loop from 10 to 100

---
+++ HelloWorldAndLoop.java
@@ -0,0 +1,11 @@
+public class HelloWorldAndLoop {
+    public static void main(String[] args) {
+        // Print Hello, World!
+        System.out.println("Hello, World! I Am Sangit Sarkar");
+
+        // Loop to print numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
